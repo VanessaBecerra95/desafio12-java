cc3758c2b26278178f2fec643d54bae00747a462
VanessaBecerra95 <vanessa.becerra.zamorano@ciisa.cl>
2024-06-10 22:56:26 -0400

    agregamos comentarios a la salida para mayor orden

--- src/Main.java
+++ src/Main.java
@@ -7,43 +7,43 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Ejercicio1");
         Marcas marcas1 = new Marcas();
-        //agregamos las marcas de bebidas
+        System.out.println("Agregamos las 10 marcas de bebidas");
         marcas1.addMarcas(Arrays.asList("Pepsi", "Coca Cola", "Fanta", "Sprite", "7up", "Bilz y Pap", "Canada Dry", "Mirinda", "Crush", "Limón Soda"));
         System.out.println(marcas1);
-        //agregamos las marcas que menciona la amiga
+        System.out.println("Agregamos las marcas que menciona la amiga");
         marcas1.addMarcas(Arrays.asList("Blokbaster", "Carrefour", "Jetix"));
         System.out.println(marcas1);
-        //cambiar blokbaster por blockbuster
+        System.out.println("Reemplazamos Blokbaster por Blockbuster");
         marcas1.replaceMarca("Blokbaster", "Blockbuster");
         System.out.println(marcas1);
-        //remover Carrefour
+        System.out.println("Removemos Carrefour");
         marcas1.removeMarca("Carrefour");
         System.out.println(marcas1);
-        //agregar posibles marcas
+        System.out.println("Agregamos posibles marcas a la lista");
         Marcas posiblesMarcas = new Marcas();
         posiblesMarcas.addMarcas(Arrays.asList("Inka cola","dr pepper"));
         marcas1.addMarcas(posiblesMarcas.getMarcas());
         System.out.println(marcas1);
-        //contar marcas1
+        System.out.println("Contamos la cantidad de marcas");
         System.out.println("Cantidad de marcas: " + marcas1.contarMarcas());
         System.out.println("------------------------------------------------------");
         System.out.println("Ejercicio2");
         Invitados invitados = new Invitados();
-        //agregamos a los invitados
+        System.out.println("Agregamos los 7 invitados");
         invitados.addInvitados(new TreeSet<>(Arrays.asList("Daniel", "Paola", "Facundo", "Pedro","Jacinta", "Florencia","Juan Pablo")));
         System.out.println(invitados);
-        //agregamos a los posibles invitados
+        System.out.println("Agregamos los posibles invitados");
         Invitados posiblesInvitados = new Invitados();
         posiblesInvitados.addInvitados(new TreeSet<>(Arrays.asList("Jorge", "Francisco", "Marcos")));
         invitados.addInvitados(posiblesInvitados.getInvitados());
         System.out.println(invitados);
-        //removemos a Jorge
+        System.out.println("Removemos a Jorge");
         invitados.removeInvitado("Jorge");
         System.out.println(invitados);
         System.out.println("------------------------------------------------------");
         System.out.println("Ejercicio3");
         Golosinas golosinas = new Golosinas();
-        //agregamos las golosinas
+        System.out.println("Agregamos las golosinas");
         golosinas.agregarGolosinas(Map.of(
                 "Chocman", 100,
                 "Trululú", 100,
@@ -54,15 +54,15 @@ public class Main {
                 "Tabletón", 5
         ));
         System.out.println(golosinas);
-        //filtrar golosinas que costaban menos de 100
+        System.out.println("Filtramos las golosinas que tienen menos de 100 unidades");
         golosinas.filtrarGolosinas(100);
         System.out.println("------------------------------------------------------");
         System.out.println("Ejercicio4");
         Juegos juegos = new Juegos();
-        //agregamos los juegos
+        System.out.println("Agregamos los juegos");
         juegos.addJuegos(new LinkedList<>(Arrays.asList("Tombo", "Congelado", "Quemaditas", "Cachipún", "Pillarse")));
         System.out.println(juegos);
-        //contar la cantidad de juegos
+        System.out.println("Contamos la cantidad de juegos");
         juegos.contarJuegos();
 
     }
